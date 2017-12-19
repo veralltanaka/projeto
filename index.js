9527bd3173333f62b105d95563b150caacc51802
vera <veralucialietanaka@hotmail.com>
2017-12-18 22:32:14 -0200

    fotos

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Titulo from './src/componente/titulo';
+import ListaCoisas from './src/componente/lista-coisas';
+
+const App = () => {
+    return (
+        <View>
+            <Titulo texto={'Lista de Compras'} />
+            <ListaCoisas />
+        </View>
+    );
+};
+
+// http://dontpad.com/munif/react
 
 AppRegistry.registerComponent('projeto', () => App);
